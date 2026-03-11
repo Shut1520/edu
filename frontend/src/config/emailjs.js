f0291486c801cba50018fc3431a242eb2a1be148
G_emma <gilwandjiemmanuel@gmail.com>
2026-03-11 13:38:19 +0100

    creation du composants de modalité de cours et creation du fichier de configuration emailjs et l'installation de la bibliothèque emailjs pour l'envoi des emails

---
+++ frontend/src/config/emailjs.js
@@ -0,0 +1,7 @@
+// Configuration EmailJS
+export const EMAILJS_CONFIG = {
+  serviceID: 'YOUR_SERVICE_ID',      // Remplace par ton Service ID
+  templateID: 'YOUR_TEMPLATE_ID',    // Remplace par ton Template ID
+  publicKey: 'YOUR_PUBLIC_KEY',      // Remplace par ta Public Key
+  adminEmail: 'admin@d-corpacademy.com'
+};
